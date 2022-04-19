fe2f0e5dafca8e1288631a93464d3a5ed6e1f63d
Cody Nicholson <codynicholson96@gmail.com>
2022-04-19 12:58:05 -0500

    day 5 start

---
+++ D4/1-SQL-Music-Tour-API_START/server.js
@@ -0,0 +1,42 @@
+// DEPENDENCIES
+const express = require('express')
+const app = express()
+const { Sequelize } = require('sequelize')
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const user = "postgres";
+const host = "localhost";
+const database = "music_tour";
+const port = "5432";
+const pass = "password";
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(database, user, pass, {
+    host,
+    port,
+    dialect: "postgres",
+    logging: false
+});
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API'
+    })
+})
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+})

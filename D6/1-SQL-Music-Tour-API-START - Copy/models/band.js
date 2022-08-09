5fe3ee87c60e4f197d8339875c44ffc387a6f752
estesthebestes <esteshunter22@gmail.com>
2022-08-09 18:58:56 -0500

    made changes to bandjs

--- D6/1-SQL-Music-Tour-API-START - Copy/models/band.js
+++ D6/1-SQL-Music-Tour-API-START - Copy/models/band.js
@@ -1,45 +1,45 @@
-'use strict'
-const {
-  Model
-} = require('sequelize');
+'use strict';
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Band extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Band.init({
-    band_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    genre: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    available_start_time: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    end_time: {
-        type: DataTypes.DATE,
-        allowNull: false
-    }
-  }, {
-    sequelize,
-    modelName: 'Band',
-    tableName: 'bands',
-    timestamps: false
-  })
-  return Band
-}
+	class Band extends Model {
+		static associate({ MeetGreet }) {
+			// meet and greets
+			Band.hasMany(MeetGreet, {
+				foreignKey: 'band_id',
+				as: 'meet_greets',
+			});
+		}
+	}
+	Band.init(
+		{
+			band_id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			genre: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+			},
+			available_start_time: {
+				type: DataTypes.DATE,
+				allowNull: false,
+			},
+			end_time: {
+				type: DataTypes.DATE,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: 'Band',
+			tableName: 'bands',
+			timestamps: false,
+		}
+	);
+	return Band;
+};
